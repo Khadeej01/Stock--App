79199c080b31a57eb93056bad2818be5f77d0a97
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-13 17:57:31 +0100

    Gitters and setters

--- src/main/java/com/Stckapp/model/ProduitModel.java
+++ src/main/java/com/Stckapp/model/ProduitModel.java
@@ -27,7 +27,66 @@ public class ProduitModel {
 	}
 
 
+	public int getId() {
+		return id;
+	}
 
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+
+	public int getPrix() {
+		return prix;
+	}
+
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+	
+	
 
 	
 	
