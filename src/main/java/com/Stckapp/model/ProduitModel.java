9cb80157e4463e8a8d3b8b4c5ce700593d413dc1
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-13 17:56:13 +0100

    Creating constroctors

--- src/main/java/com/Stckapp/model/ProduitModel.java
+++ src/main/java/com/Stckapp/model/ProduitModel.java
@@ -9,7 +9,22 @@ public class ProduitModel {
 	private String categorie ;
 	
 	
-
+	public ProduitModel (int id, String nom,  String description, int quantite , int prix , String categorie) {
+	
+	this.id = id;
+	this.nom = nom;
+	this.description = description;
+	this.quantite = quantite;
+	this.prix = prix;
+	this.categorie = categorie; 
+}
+	public ProduitModel ( String nom,  String description, int quantite , int prix , String categorie) {
+		this.nom = nom;
+		this.description = description;
+		this.quantite = quantite;
+		this.prix = prix;
+		this.categorie = categorie; 
+	}
 
 
 
