e773fef22cbe60002690fb5aa8e70d8305036ad6
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-13 17:54:40 +0100

    starting java part

---
+++ src/main/java/com/Stckapp/model/ProduitModel.java
@@ -0,0 +1,20 @@
+package com.Stckapp.model;
+
+public class ProduitModel {
+	private int id ;
+	private String nom;
+	private String description;
+	private int quantite ;
+	private int prix ;
+	private String categorie ;
+	
+	
+
+
+
+
+
+	
+	
+}
+ 
