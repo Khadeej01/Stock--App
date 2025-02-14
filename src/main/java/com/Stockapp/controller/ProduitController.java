cd25681997686864f7efa22ba51426b479486f49
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-14 16:29:16 +0100

    ProduitController class

--- src/main/java/com/Stockapp/controller/ProduitController.java
+++ src/main/java/com/Stockapp/controller/ProduitController.java
@@ -15,7 +15,43 @@ import com.Stckapp.DAO.ProdutDAO;
 import com.Stckapp.model.ProduitModel;
 @WebServlet("/")
 
+public class ProduitController extends HttpServlet {
+	private ProdutDAO prDAO;
 
+    public void init() {
+    	prDAO = new ProdutDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+               
+                case "/insert":
+                	insertProduit(request, response);
+                    break;
+                    
+                case "/":
+                	listProduit(request, response);
+                	break;
+
+               
+                default:
+                	listProduit(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+   
 
 
 
